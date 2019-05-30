6a4745ef0fcd322c6d619611063b78d5f8ec8220
David Ragsdale <me@davidjragsdale.com>
2019-05-30 09:07:33 +0100

    Add dynamic pricing view

--- routes/cart.js
+++ routes/cart.js
@@ -25,9 +25,12 @@ const getOptions = req => {
   const currencySymbol = req.query.currency
   const currency = getCurrency(currencySymbol)
 
+  const isAnnual = isTrue(req.query.annual)
+
   return {
     currency,
     currencySymbol,
+    isAnnual,
   }
 }
 
@@ -35,8 +38,43 @@ module.exports = router.get('/', (req, res) => {
   const options = getOptions(req)
   debug('route options', options)
 
+  const pricingStructure = {
+    free: {
+      annually: '$0',
+      monthly: '$0',
+    },
+    pro: {
+      annually: '$150',
+      monthly: '$15',
+    },
+    enterprise: {
+      annually: '$250',
+      monthly: '$29',
+    },
+  }
+
   const data = {
     ...options,
+    prices: {
+      free: {
+        amount: options.isAnnual
+          ? pricingStructure.free.annually
+          : pricingStructure.free.monthly,
+        term: options.isAnnual ? 'yr' : 'mo',
+      },
+      pro: {
+        amount: options.isAnnual
+          ? pricingStructure.pro.annually
+          : pricingStructure.pro.monthly,
+        term: options.isAnnual ? 'yr' : 'mo',
+      },
+      enterprise: {
+        amount: options.isAnnual
+          ? pricingStructure.enterprise.annually
+          : pricingStructure.enterprise.monthly,
+        term: options.isAnnual ? 'yr' : 'mo',
+      },
+    },
   }
 
   if (isTrue(req.query.json)) {
